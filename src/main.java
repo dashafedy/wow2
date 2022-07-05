392f1c9c60aa88a9c345eb6666083168aec7ba62
Dasha Fedy <dashaafedy@gmail.com>
2022-07-05 21:49:57 +0300

    add files

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 9_000;
+
+        int x = 20;
+        int y = amount / x;
+
+
+        System.out.println(y + " миль начисленно за купленный билет стоимость " + (amount) );
+
+
+
+    }
+}
