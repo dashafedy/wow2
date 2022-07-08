1353b451e31700d3b94e9c6a1e28bdc10b4cc59f
Dasha Fedy <dashaafedy@gmail.com>
2022-07-08 14:22:42 +0300

    add changes

--- src/main.java
+++ src/main.java
@@ -2,12 +2,11 @@ public class main {
     public static void main(String[] args) {
         int amount = 9_000;
 
-        int x = 20;
-        int y = amount / x;
+        int x = 20; // количесво рублей потрачено на билет
+        int bonus = amount / x; // количесвто бонусных миль начислено
 
 
-        System.out.println(y + " миль начисленно за купленный билет стоимость " + (amount) );
-
+        System.out.println(bonus + " миль начисленно за купленный билет стоимость " + (amount));
 
 
     }
