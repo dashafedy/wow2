ce6031fb971f7911a77c8e432e3da96ef1fea18d
Dasha <107143963+dashafedy@users.noreply.github.com>
2022-07-08 14:24:48 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -2,11 +2,11 @@ public class main {
     public static void main(String[] args) {
         int amount = 9_000;
 
-        int x = 20; // количесво рублей потрачено на билет
-        int bonus = amount / x; // количесвто бонусных миль начислено
+        int x = 20; // количество рублей потрачено на билет
+        int bonus = amount / x; // количество бонусных милль начисленно
 
 
-        System.out.println(bonus + " миль начисленно за купленный билет стоимость " + (amount));
+        System.out.println(bonus + " милль начисленно за купленный билет стоимость " + (amount));
 
 
     }
